3169df0ba4ee45fed7cef578aa29453063493507
laian <tanlaian2004@gmail.com>
2023-02-11 22:34:44 +0800

    readline

--- headers/minishell.h
+++ headers/minishell.h
@@ -2,9 +2,15 @@
 
 #define MINISHELL_H
 
-#include <stdio.h>
-
-
-
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <string.h>
+# include <limits.h>
+# include <signal.h>
+# include <termios.h>
 
 #endif
--- srcs/msh_main.c
+++ srcs/msh_main.c
@@ -1,2 +1,31 @@
-#include "minishell.h"
+#include "../headers/minishell.h"
 
+void	handle_line()
+{
+	int		stop_sig;
+	char	*line;
+
+	stop_sig = 0;
+	while (stop_sig == 0)
+	{
+		line = readline("> ");
+		if (line && *line)
+		{
+			add_history(line);
+			rl_redisplay();
+		}
+		// handle_cmd(line);
+	}
+	clear_history();
+	free(line);
+	return ;
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	(void) argv, envp;
+	if (argc != 1) // args check
+		return (0);
+	handle_line();
+	
+}
