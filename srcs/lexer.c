b7655916da3f444e66c433fcef608597f81de76d
Please Remember To Change In the Future <sexamogus@notarealemail.com>
2023-05-25 22:50:21 +0800

    lexer is (hopefully) working

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -33,9 +33,17 @@ int	is_token(char c)
 }
 
 // hmm would this work
-# define DO_NOT_IGNORE_SPACE !(*ignore_space) 
+// you know this could be flags, but like im lazy so
 
-int	find_token_pos(char *line, int *index_pair, int *ignore_space)
+// also god i hate these terms, will change to better ones later
+# define DO_NOT_IGNORE_SPACE	0
+# define IGNORE_SPACE			1
+# define STOP_ON_SPACE			2
+# define IF_IGNORE_SPACE		(*have_spaces) == IGNORE_SPACE
+# define IF_DO_NOT_IGNORE_SPACE	(*have_spaces) == DO_NOT_IGNORE_SPACE
+# define IF_STOP_ON_SPACE		(*have_spaces) == STOP_ON_SPACE
+
+int	find_token_pos(char *line, int *index_pair, int *have_spaces)
 {
 	int		i;
 	int		tk_type;
@@ -43,13 +51,15 @@ int	find_token_pos(char *line, int *index_pair, int *ignore_space)
 	// check if the index is on '\0' symbol, if it is kill it with fire
 	if (index_pair[1] + 1 >= ft_strlen(line))
 		return (0);
-	i = index_pair[1] + 1;
 	index_pair[0] = index_pair[1] + 1;
+	i = index_pair[0];
 	// fuck you bunny ears
-	while (line[i] != '\0' && line[i] == ' ')
-		i++;
-	if (DO_NOT_IGNORE_SPACE)
+	if (IF_DO_NOT_IGNORE_SPACE)
+	{
+		while (line[i] != '\0' && line[i] == ' ')
+			i++;
 		index_pair[0] = i;
+	}
 	tk_type = is_token(line[i]);
 	// handles identifier
 	if (tk_type != 0)
@@ -64,24 +74,30 @@ int	find_token_pos(char *line, int *index_pair, int *ignore_space)
 		// handles $
 		else if (line[i + 1] != '\0' && tk_type == 6)
 		{
-			// handles $?
 			if (line[i + 1] == '?')
 				i++;
+			else
+				*have_spaces = STOP_ON_SPACE;
 		}
 		if (line[i] == '\'' || line[i] == '\"')
 			// flip flop
-			*ignore_space = (*ignore_space == 0);
+			*have_spaces = !(*have_spaces == IGNORE_SPACE);
 	}
 	// handles everything else
 	else
 	{
-		while (line[i + 1] != '\0' && !is_token(line[i + 1]))
+		while (line[i] != '\0' && !is_token(line[i]))
 		{
 			// this is a pain in the ass
-			if (line[i + 1] == ' ' && DO_NOT_IGNORE_SPACE)
+			if (line[i] == ' ' && (IF_DO_NOT_IGNORE_SPACE || IF_STOP_ON_SPACE))
+			{	
+				if (IF_STOP_ON_SPACE)
+					*have_spaces = IGNORE_SPACE;
 				break;
+			}
 			i++;
 		}
+		--i;
 	}
 	index_pair[1] = i;
 	return (1);
@@ -96,9 +112,9 @@ int	lexer(t_data *data)
 	int		token_pos[2];
 	char	*new_token;
 	
-	int		ignore_space;
+	int		have_space;
 
-	ignore_space = 0;
+	have_space = DO_NOT_IGNORE_SPACE;
 	len = ft_strlen(data->line);
 	data->tokens = (char **) malloc (sizeof(char *));
 	if (!data->tokens)
@@ -107,7 +123,7 @@ int	lexer(t_data *data)
 	data->tokens[0] = NULL;
 	while (1)
 	{
-		if (!find_token_pos(data->line, token_pos, &ignore_space) || token_pos[1] >= len)
+		if (!find_token_pos(data->line, token_pos, &have_space) || token_pos[1] >= len)
 			break ;
 		new_token = ft_substr(data->line, token_pos[0], token_pos[1]);
 		data->tokens = realloc_append(data->tokens, new_token);
