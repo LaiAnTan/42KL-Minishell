91f90805145ee49351e5a6c43de7e464df70c10b
Please Remember To Change In the Future <sexamogus@notarealemail.com>
2023-05-27 23:20:02 +0800

    pipe is done

--- srcs/cmd.c
+++ srcs/cmd.c
@@ -1,18 +1,112 @@
 #include "../headers/minishell.h"
 
-void	run_cmd(t_data *data)
+void	single_command(t_data *data, t_list *cmds)
 {
 	char	*cmd;
 	char	**cmd_paths;
 
-	cmd = ft_strdup(data->cmds->cmd.cmd[0]);
+	cmd = ft_strdup(cmds->cmd.cmd[0]);
 	cmd_paths = get_cmd_path(data, cmd);
-	// while (data != NULL)
-	// {
-	if (handle_builtins(cmd, data->cmds->cmd.cmd, data) == 0)
-		exec_cmd(data, cmd_paths, data->cmds->cmd.cmd);
-	// 	data->cmds = data->cmds->next;
-	// }
+	if (handle_builtins(cmd, cmds->cmd.cmd, data) == 0)
+		exec_cmd(data, cmd_paths, cmds->cmd.cmd);
+}
+
+// why do we not have a command count
+int		get_command_count(t_data *data)
+{
+	t_list	*counter;
+	int		ret;
+
+	ret = 0;
+	counter = data->cmds;
+	while (counter)
+	{
+		++ret;
+		counter = counter->next;
+	}
+	return (ret);
+}
+
+void	multiple_commands(t_data *data)
+{
+	int	dispatched;
+	int	cmd_count;
+	// 0 - read 1 - write
+	int	pipe_storage[2];
+	int	prev_pipe;
+
+	dispatched = 0;
+	cmd_count = get_command_count(data);
+	while (dispatched < cmd_count)
+	{
+		// when t_list have no init function
+		data->cmds->in_fd = STDIN_FILENO;
+		data->cmds->out_fd = STDOUT_FILENO;
+
+		// piping
+		if (!dispatched)
+		{
+			pipe(pipe_storage);
+			data->cmds->out_fd = pipe_storage[1];
+		}
+		else if (dispatched == cmd_count - 1) // last command
+		{
+			data->cmds->in_fd = prev_pipe;
+		}
+		else // everything in between
+		{
+			data->cmds->in_fd = prev_pipe;
+			pipe(pipe_storage);
+			data->cmds->out_fd = pipe_storage[1];
+		}
+
+		if (!fork())
+		{
+			close(pipe_storage[0]);
+
+			dup2(data->cmds->in_fd, STDIN_FILENO);
+			dup2(data->cmds->out_fd, STDOUT_FILENO);
+
+			single_command(data, data->cmds);
+			// i fix this later
+			exit(0);
+		}
+		else
+		{
+			// cleaning up file desc
+			if (!dispatched)
+			{
+				close(pipe_storage[1]);
+				prev_pipe = pipe_storage[0];
+			}
+			else if (dispatched == cmd_count - 1)
+			{
+				close(prev_pipe);
+			}
+			else
+			{
+				close(prev_pipe);
+				close(pipe_storage[1]);
+				prev_pipe = pipe_storage[0];
+			}
+			++dispatched;
+			data->cmds = data->cmds->next;
+		}
+	}
+	while (cmd_count)
+	{
+		// hey replace NULL with the saved exit code thanks
+		waitpid(0, NULL, 0);
+		--cmd_count;
+	}
+}
+
+void	run_cmd(t_data *data)
+{
+	if (data->cmds->next)
+		multiple_commands(data);
+	else
+		single_command(data, data->cmds);
 }
 
 int	exec_cmd(t_data *data, char **cmd_paths, char **args)
