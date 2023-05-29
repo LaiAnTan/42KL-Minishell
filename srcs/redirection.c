adeba76f23e55a15b1fadd04203dd79fa0ff6f6d
laian <tanlaian2004@gmail.com>
2023-05-29 13:43:27 +0800

    structure done but not working

--- srcs/redirection.c
+++ srcs/redirection.c
@@ -24,62 +24,15 @@ output: only file c is redirected as input
 eg: cat < a << EOF < c
 output: STDIN is read until EOF is found, only then file c is redirected as input
 
+eg: cat < a b
+output: b is output since cat reads from parameter first 
+
 for multiple commands:
 redirections should be done inside the child process, 
 but before the alteration of the command's input and output
 this is because redirections override piping
 
 */
-
-void	handle_redir_input(char *filename, int *in_fd)
-{
-	int		fd;
-
-	fd = open(filename, O_RDONLY, 0777);
-	if (fd == -1)
-	{
-		printf("%s: file could not be open", filename); // handle later
-		return (0);
-	}
-	dup2(fd, dup(STDIN_FILENO));
-	close(fd);
-	return (1);
-}
-
-void	handle_redir_input_heredoc(char *delimiter, int *in_fd)
-{
-	// using readline
-}
-
-void	handle_redir_output(char *filename, int *out_fd)
-{
-	int	fd;
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	if (fd == -1)
-	{
-		printf("%s: file could not be open", filename); // handle later
-		return (0);
-	}
-	dup2(fd, dup(STDOUT_FILENO));
-	close(fd);
-	return (1);
-}
-
-void	handle_redir_output_append(char *filename, int *out_fd)
-{
-	int	fd;
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0777);
-	if (fd == -1)
-	{
-		printf("%s: file could not be open", filename); // handle later
-		return (0);
-	}
-	dup2(fd, dup(STDOUT_FILENO));
-	close(fd);
-	return (1);
-}
 
 int		is_redirect(char *arg)
 {
@@ -91,22 +44,75 @@ int		is_redirect(char *arg)
 	return (0);
 }
 
+int	handle_redir_input(char *filename, int *in_fd)
+{
+	int		fd;
+
+	if (is_redirect(filename))
+		return (-1);
+	fd = open(filename, O_RDONLY, 0777);
+	if (fd == -1)
+	{
+		printf("%s: file could not be open", filename); // handle later
+		return (-1);
+	}
+	dup2(fd, *in_fd);
+	close(fd);
+	return (1);
+}
+
+int	handle_redir_input_heredoc(char *delimiter, int *in_fd)
+{
+	if (is_redirect(delimiter))
+		return (-1);
+	// using readline or gnl
+}
+
+int	handle_redir_output(char *filename, int *out_fd)
+{
+	int	fd;
+
+	if (is_redirect(filename))
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd == -1)
+	{
+		printf("%s: file could not be open", filename); // handle later
+		return (-1);
+	}
+	dup2(fd, *out_fd);
+	close(fd);
+	return (1);
+}
+
+int	handle_redir_output_append(char *filename, int *out_fd)
+{
+	int	fd;
+
+	if (is_redirect(filename))
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0777);
+	if (fd == -1)
+	{
+		printf("%s: file could not be open", filename); // handle later
+		return (-1);
+	}
+	dup2(fd, *out_fd);
+	close(fd);
+	return (1);
+}
+
 int		contains_redirect(char **args)
 {
 	int	i;
-	int	flag;
 
 	i = 0;
-	flag = 0;
-
-	while (args[i + 1] != NULL)
+	while (args[i] != NULL)
 	{
 		if (is_redirect(args[i]))
-			flag = 1;
+			return (1);
 		i++;
 	}
-	if (flag == 1)
-		return (1);
 	return (0);
 }
 
@@ -146,54 +152,100 @@ int		*get_next_redirect(char **args, int index)
 	return (redirect_info);
 }
 
+int		count_args_without_redirect(char **args)
+{
+	int		i;
+	int		len;
+	int		count;
+	
+	i = 0;
+	len = count_2d_array(args);
+	count = 0;
+	while (args[i] != NULL)
+	{
+		if (is_redirect(args[i]))
+		{
+			if (i == len)
+				break ;
+			i += 2;
+		}
+		else
+		{
+			++i;
+			++count;
+		}
+	}
+	return (count);
+}
+
 char	**get_cmd_args_without_redirect(char **args)
 {
 	int		i;
-	int		*redirect_info;
-
+	int		j;
+	int		old_len;
+	int		new_len;
 	char	**new;
 
-	i = 0;
-	redirect_info = get_next_redirect(args, 0);
-	if (redirect_info == NULL)
+	if (contains_redirect(args) == 0)
 		return (args);
-	new = (char **) malloc(sizeof(char *) * redirect_info[1]);
-	while (i < redirect_info[1]);
+	i = 0;
+	j = 0;
+	old_len = count_2d_array(args);
+	new_len = count_args_without_redirect(args);
+	new = malloc (sizeof(char *) * new_len + 1);
+	while (args[i] != NULL)
 	{
-		new[i] = ft_strdup(args[i]);
-		++i;
+		if (is_redirect(args[i]) == 0)
+		{
+			if (i == old_len)
+				break ;
+			i += 2;
+		}
+		else
+		{
+			new[j] = ft_strdup(args[i]);
+			++i;
+			++j;
+		}
 	}
-	new[i] = NULL;
+	new[new_len] = NULL;
+	free_2d_array(&args);
 	return (new);
 }
 
 int		handle_redirect(char **args, int *in_fd, int *out_fd)
 {
 	int		i;
+	int		error;
 	int		*redirect_info; // 0 - type 1 - index
 
 	i = 0;
-	if (!contains_redirect(args))
+	error = 0;
+	if (contains_redirect(args) == 0)
 		return (0);
 	while (args[i] != NULL)
 	{
 		redirect_info = get_next_redirect(args, i);
 
 		if (redirect_info == NULL)
-			return (0); // handle error
+			return (0); // error
 		else if (redirect_info[0] == -1 && redirect_info[1] == -1)
-			return (1); // handle done
-		else if (args[i] == NULL)
-			return (1); // double done
+			return (1); // done
+		
 		if (redirect_info[0] == 1)
-			handle_redir_input(args[i + 1], in_fd);
+			error = handle_redir_input(args[i + 1], in_fd);
 		else if (redirect_info[0] == 2)
-			handle_redir_output(args[i + 1], out_fd);
+			error = handle_redir_output(args[i + 1], out_fd);
 		else if (redirect_info[0] == 3)
-			handle_redir_input_heredoc(args[i + 1], in_fd);
+			error = handle_redir_input_heredoc(args[i + 1], in_fd);
 		else if (redirect_info[0] == 4)
-			handle_redir_output_append(args[i + 1], out_fd);
+			error = handle_redir_output_append(args[i + 1], out_fd);
+
+		if (error == -1)
+			return (0); // error
+
 		i = redirect_info[1] + 1;
 		free(redirect_info);
 	}
+	return (1); // done
 }
