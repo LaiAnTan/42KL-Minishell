4bdd6c6d7ef4f35e298f1e5f376b2fd0b3b10132
laian <tanlaian2004@gmail.com>
2023-05-29 19:01:39 +0800

    bruh

--- srcs/utilities.c
+++ srcs/utilities.c
@@ -56,7 +56,7 @@ int	ft_strcmp(char *s1, char *s2)
 	// maybe i should include a failswitch as well~
 	if (!s1 || !s2)
 		return (!(!s1 && !s2));
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
 			return (1);
