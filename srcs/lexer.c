2547a1dfe80d892a90b7868a09468bd97d6725d4
Please Remember To Change In the Future <sexamogus@notarealemail.com>
2023-05-29 23:05:10 +0800

    fuck me fuck me

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -80,7 +80,7 @@ int	find_token_pos(char *line, int *index_pair)
 	// handles everything else
 	else
 	{
-		while (line[i + 1] != '\0' && line[i + 1] != ' ' && !is_token(line[i]))
+		while (line[i + 1] != '\0' && line[i + 1] != ' ' && !is_token(line[i + 1]))
 			i++;
 	}
 	index_pair[1] = i;
