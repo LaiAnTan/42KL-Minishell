1c0da5b9419ac22c81b07722b81ca55d19f4494b
laian <tanlaian2004@gmail.com>
2023-05-30 16:27:34 +0800

    cleanup

--- srcs/builtins/builtins_unset.c
+++ srcs/builtins/builtins_unset.c
@@ -11,7 +11,7 @@ int builtin_unset(char **args, t_data *data)
 	lst = data->vars;
 	while (args[i] != NULL)
 	{
-		if (ft_strcmp(args[i], "_"))
+		if (ft_strcmp(args[i], "_") == 0)
 			i++;
 		node = find_var(lst, args[i]);
 		if (node != NULL)
