1a764fc4b2e8283fdf987b956244606b0e290ebb
Please Remember To Change In the Future <sexamogus@notarealemail.com>
2023-06-01 20:38:57 +0800

    added explanation

--- srcs/cmd.c
+++ srcs/cmd.c
@@ -81,13 +81,6 @@ void	multiple_commands(t_data *data)
 	cmd_count = get_command_count(data);
 	while (dispatched < cmd_count)
 	{
-		// if (dispatched)
-		// 	data->cmds->in_fd = prev_pipe;
-		// if (dispatched != cmd_count - 1)
-		// {
-		// 	pipe(pipe_storage);
-		// 	data->cmds->out_fd = pipe_storage[1];
-		// }
 		if (!dispatched)
 		{
 			printf("first\n");
@@ -108,6 +101,10 @@ void	multiple_commands(t_data *data)
 		last_child_pid = fork();
 		if (last_child_pid == 0)
 		{
+			// AHHH PISS SHIT
+
+			// DO NOT CLOSE THE READ END OF THE PIPE IF IT IS THE LAST COMMAND
+			// LAST COMMAND LITERALLY READ FROM THE READ END OF THE PREVIOUS PIPE, AND PIPE_STORAGE ISNT REPIPED IN THE LAST COMMAND
 			if (!(dispatched == cmd_count - 1))
 				close(pipe_storage[0]);
 			if (handle_redirect(data->cmds->cmd.cmd, &data->cmds->in_fd, &data->cmds->out_fd) == -1)
@@ -122,13 +119,6 @@ void	multiple_commands(t_data *data)
 		}
 		else
 		{
-			// if (dispatched)
-			// 	close(prev_pipe);
-			// if (dispatched != cmd_count - 1)
-			// {
-			// 	close(pipe_storage[1]);
-			// 	prev_pipe = pipe_storage[0];
-			// }
 			if (!dispatched) 
 			{
 				printf("first\n");
