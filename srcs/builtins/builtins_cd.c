49080c16f076a9ed086addac35c65ff59c76a1ca
laian <tanlaian2004@gmail.com>
2023-06-03 19:59:52 +0800

    fixed, some fucking shit

--- srcs/builtins/builtins_cd.c
+++ srcs/builtins/builtins_cd.c
@@ -47,10 +47,7 @@ int	builtin_cd(char **args, t_data *data)
 			ft_lstadd_back(&data->vars, node);
 		}
 		else
-		{
-			free(node->env.str);
 			node->env.str = ft_append(ft_strdup("OLDPWD="), data->cwd);
-		}
 		free(data->cwd);
 		data->cwd = getcwd(NULL, PATH_MAX);
 		node = find_var(data->vars, "PWD");
@@ -60,10 +57,7 @@ int	builtin_cd(char **args, t_data *data)
 			ft_lstadd_back(&data->vars, node);
 		}
 		else
-		{
-			free(node->env.str);
 			node->env.str = ft_append(ft_strdup("PWD="), data->cwd);
-		}
 		data->last_exit = 0;
 	}
 	else
