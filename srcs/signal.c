6f867917e4b4f8f91c6559ba5001a7bed5b851c4
I did not remember to change this :P <no way someone has this email wadjojwaosdjoadjwa@gmail.com>
2023-06-13 17:36:23 +0800

    e

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,17 +6,17 @@
 /*   By: cshi-xia <cshi-xia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 10:31:02 by tlai-an           #+#    #+#             */
-/*   Updated: 2023/06/13 17:16:16 by cshi-xia         ###   ########.fr       */
+/*   Updated: 2023/06/13 17:36:16 by cshi-xia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
-void	modify_attr(t_data *data)
-{
-	data->attr->mod_attributes.c_iflag &= ~ECHOCTL;
-	tcsetattr(STDIN_FILENO, TCSANOW, &data->attr->mod_attributes);
-}
+// void	modify_attr(t_data *data)
+// {
+// 	data->attr->mod_attributes.c_iflag &= ~ECHOCTL;
+// 	tcsetattr(STDIN_FILENO, TCSANOW, &data->attr->mod_attributes);
+// }
 
 void	new_line_handler(int sig_code)
 {
